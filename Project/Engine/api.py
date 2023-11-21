7f47fd85e952b3d67093a2c1461e1a753deed0da
julessilvestri <jules.sil05@gmail.com>
2023-11-21 10:19:36 +0100

    push fix bug

--- Project/Engine/api.py
+++ Project/Engine/api.py
@@ -224,21 +224,8 @@ def getStatusArena(round):
 
 @app.route('/kafka', methods=['GET'])
 def endPoint():
-    props = read_ccloud_config("client.properties")
-    props["group.id"] = "python-group-1"
-    props["auto.offset.reset"] = "earliest"
-
-    consumer = Consumer(props)
-    consumer.subscribe(["my-topic"])
-    try:
-        while True:
-            msg = consumer.poll(1.0)
-            if msg is not None and msg.error() is None:
-                print("key = {key:12} value = {value:12}".format(key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
-    except KeyboardInterrupt:
-        pass
-    finally:
-        consumer.close()
+    # Faire la methode kafka
+    return "kafka"
 
 if __name__ == "__main__" :
     engine = Engine()
