87f7b7a284a6c7918553bc4538f4ab33b4fe4ab9
julessilvestri <jules.sil05@gmail.com>
2023-11-21 10:13:36 +0100

    push swagger function

--- Project/Engine/api.py
+++ Project/Engine/api.py
@@ -6,9 +6,12 @@ from character import *
 import threading
 from kafka import KafkaConsumer
 from kafka.errors import NoBrokersAvailable
+from kafka.consumer.fetcher import ConsumerRecord
 import json
+from flasgger import Swagger
 
 app = flask.Flask(__name__)
+swagger = Swagger(app)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -21,6 +24,43 @@ def home():
 # Executer le script requests.py pour les méthodes POST 
 @app.route('/character/join', methods=['POST'])
 def addCharacterToArena():
+    """
+    Add a new character to the arena.
+
+    ---
+    tags:
+      - POST
+    parameters:
+      - name: characterData
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            cid:
+              type: string
+              description: ID of the character
+            team:
+              type: string
+              description: Team of the character
+            life:
+              type: integer
+              description: Life points of the character
+            strength:
+              type: integer
+              description: Strength of the character
+            armor:
+              type: integer
+              description: Armor of the character
+            speed:
+              type: integer
+              description: Speed of the character
+    responses:
+      200:
+        description: Character added to the arena successfully
+      500:
+        description: Internal server error
+    """
     try:
         # Initialiser les players avec les données JSON de la requête
         characterData = request.get_json()
@@ -43,6 +83,34 @@ def addCharacterToArena():
 #         /character/action/ - cid, action, target
 @app.route('/character/action/', methods=['POST'])
 def addActionToPlayer():
+    """
+    Add an action to a character in the arena.
+
+    ---
+    tags:
+      - POST
+    parameters:
+      - name: actionData
+        in: body
+        required: true
+        schema:
+          type: object
+          properties:
+            cid:
+              type: string
+              description: ID of the character
+            action:
+              type: string
+              description: Action to be performed by the character
+            target:
+              type: string
+              description: Target of the action
+    responses:
+      200:
+        description: Action added to the character successfully
+      500:
+        description: Internal server error
+    """
     try:
         # Récupérer les données JSON de la requête
         actionData = request.get_json()
@@ -74,6 +142,24 @@ def addActionToPlayer():
 # Exemple requete = http://127.0.0.1:5000/character/Jules
 @app.route('/character/<cid>', methods=['GET'])
 def getPlayerById(cid):
+    """
+    Get player information by ID.
+
+    ---
+    tags:
+      - GET
+    parameters:
+      - name: cid
+        in: path
+        type: string
+        required: true
+        description: ID of the player
+    responses:
+      200:
+        description: Player information
+      404:
+        description: Player not found
+    """
     try:
         playerList = [player.toDict() for player in arena._playersList]
         player = next((p for p in playerList if p["cid"] == cid), None)
@@ -89,6 +175,18 @@ def getPlayerById(cid):
 # Exemple requete = http://127.0.0.1:5000/characters/
 @app.route('/characters/', methods=['GET'])
 def getAllPlayer():
+    """
+    Get information for all players.
+
+    ---
+    tags:
+      - GET
+    responses:
+      200:
+        description: Information for all players
+      500:
+        description: Internal server error
+    """
     try:
         playerList = [player.toDict() for player in arena._playersList]
         return jsonify(playerList)
@@ -101,6 +199,24 @@ def getAllPlayer():
 # Exemple requete = http://127.0.0.1:5000/status/5 => Donne les infos sur le round 5
 @app.route('/status/<round>', methods=['GET'])
 def getStatusArena(round):
+    """
+    Get the status of the arena for a specific round.
+
+    ---
+    tags:
+      - GET
+    parameters:
+      - name: round
+        in: path
+        type: string
+        required: true
+        description: The round for which to get the arena status
+    responses:
+      200:
+        description: Arena status for the specified round
+      500:
+        description: Internal server error
+    """
     try:
         return jsonify(engine.getState())
     except Exception as e:
@@ -108,15 +224,21 @@ def getStatusArena(round):
 
 @app.route('/kafka', methods=['GET'])
 def endPoint():
+    props = read_ccloud_config("client.properties")
+    props["group.id"] = "python-group-1"
+    props["auto.offset.reset"] = "earliest"
+
+    consumer = Consumer(props)
+    consumer.subscribe(["my-topic"])
     try:
-        consumer = KafkaConsumer(bootstrap_servers='pkc-lzvrd.us-west4.gcp.confluent.cloud:9092', auto_offset_reset='earliest', value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-        consumer.subscribe(['topicJules'])
-        message = next(consumer)
-        return jsonify(message.value)
-    except NoBrokersAvailable as e:
-        return jsonify({"error": f"NoBrokersAvailable - {str(e)}"}), 500
-    except Exception as e:
-        return jsonify({"error": f"Autre erreur - {str(e)}"}), 500
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is not None and msg.error() is None:
+                print("key = {key:12} value = {value:12}".format(key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close()
 
 if __name__ == "__main__" :
     engine = Engine()
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# agentBattleRoyal
+# agentBattleArena
+
+To start : 
+    - Run api.py (in /Engine folder)
+    - Run request.py (in /api folder) for add data
+
+To access to method => 127.0.0.1:5000
+To access to swagger => 127.0.0.1:5000/apidocs
