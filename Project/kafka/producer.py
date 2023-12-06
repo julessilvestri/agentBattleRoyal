32dbb4b16bf342e83aa13e436e317585a4a208de
julessilvestri <jules.sil05@gmail.com>
2023-12-06 19:38:30 +0100

    push consumer, non fonctionnel

--- Project/kafka/producer.py
+++ Project/kafka/producer.py
@@ -1,4 +1,5 @@
 from confluent_kafka import Producer
+import json
 
 conf_cloud = {
     'bootstrap.servers': 'pkc-6ojv2.us-west4.gcp.confluent.cloud:9092',
@@ -13,8 +14,11 @@ producer = Producer(conf_cloud)
 
 topic = 'topicKafka'
 message_key = 'cle_du_message'
-message_value = 'Contenu du message'
+
+with open('../Engine/data.json') as f:
+    json_content = json.load(f)
+
+message_value = json.dumps(json_content)
 
 producer.produce(topic, key=message_key, value=message_value)
 producer.flush()
-print("fin")
